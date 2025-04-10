28942e8422b18fc60df05d7f3110fae000f6f0f3
Arthur Santoro <arthursantorogomes92@gmail.com>
2025-04-09 21:50:45 -0400

    sim

--- exercicios_e_exemplosAulas/exemplos.py
+++ exercicios_e_exemplosAulas/exemplos.py
@@ -1,3 +1,11 @@
-a = [88,81,82,83]
-a.insert(1,100)
-print(a)
+salario = float(input("salario: "))
+
+if salario < 500:
+    reajuste = salario + ( salario * 0.15)
+    print(f"salario com reajuste: {reajuste}")
+elif salario <= 1000:
+    reajuste = salario + ( salario * 0.10)
+    print(f"salario com reajuste: {reajuste}")
+elif salario > 1000:
+    reajuste = salario + ( salario * 0.05)
+    print(f"salario com reajuste: {reajuste}")
