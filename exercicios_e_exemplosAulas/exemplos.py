5f33022614d517ce80596ee8d40347932e152ecf
Arthur Santoro <arthursantorogomes92@gmail.com>
2025-04-10 21:46:59 -0400

    hihi

--- exercicios_e_exemplosAulas/exemplos.py
+++ exercicios_e_exemplosAulas/exemplos.py
@@ -1,11 +1,15 @@
-salario = float(input("salario: "))
-
-if salario < 500:
-    reajuste = salario + ( salario * 0.15)
-    print(f"salario com reajuste: {reajuste}")
-elif salario <= 1000:
-    reajuste = salario + ( salario * 0.10)
-    print(f"salario com reajuste: {reajuste}")
-elif salario > 1000:
-    reajuste = salario + ( salario * 0.05)
-    print(f"salario com reajuste: {reajuste}")
+num1 = int(input("num1: "))
+num2 = int(input("num1: "))
+num3 = int(input("num1: "))
+if num1 > num2 and num1 > num3 and num2 > num3:
+    print(f"maior: {num1}, menor: {num3}")
+elif num1 > num2 and num1 > num3 and num3 > num2:
+    print(f"maior: {num1}, menor: {num2}")
+elif num2 > num1 and num2 > num3 and num1 > num3:
+    print(f"maior: {num2}, menor: {num3}")
+elif num2 > num1 and num2 > num3 and num3 > num1:
+    print(f"maior: {num2}, menor: {num1}")
+elif num3 > num1 and num3 > num2 and num1 > num2:
+    print(f"maior: {num3}, menor: {num2}")
+elif num3 > num1 and num3 > num2 and num2 > num1:
+    print(f"maior: {num3}, menor: {num1}")
