d5bdfdcea11ab557cb96d2705b6dab4fd1cd41c7
Arthur Santoro <arthursantorogomes92@gmail.com>
2025-05-06 19:38:24 -0400

    pronto toronto

--- exercicios_e_exemplosAulas/exemplos.py
+++ exercicios_e_exemplosAulas/exemplos.py
@@ -2,7 +2,7 @@ listaClientes=[]
 
 print("\nBem Vindo ao Banco!\n")
 while True:
-    print("Ações disponíveis:\n \n 1- Cadastrar Cliente\n 2- Consultar saldo\n 3- Fazer depósito\n 4- Fazer saque\n")
+    print("Ações disponíveis:\n \n 1- Cadastrar Cliente\n 2- Consultar saldo\n 3- Fazer depósito\n 4- Fazer saque\n 5- Sair.")
     print("digite o número respectivo a ação desejada\n")
     acao = int(input("Ação: "))
     if acao == 1:
@@ -32,5 +32,27 @@ while True:
             for cpf in i:
                 if cpf == buscaCPF:
                     print(f"Saldo de {i[0]}: {i[6]} reais")
-                else:
-                    print("CPF incorreto ou não cadastrado.")        
+    elif acao == 3:
+        print("Digite o CPF da conta que você deseja efetuar o depósito.\n")
+        buscaCPF = int(input("CPF: "))
+        for i in listaClientes:
+            for cpf in i:
+                if cpf == buscaCPF:
+                    print(f"Digite o valor para depositar na conta de ",i[0])
+                    valor = float(input("\nValor: "))
+                    i[6] += valor
+                    print(f"Novo saldo: {i[6]}")
+    elif acao == 4:
+        for i in listaClientes:
+            for cpf in i:
+                if cpf == buscaCPF:
+                    print(f"Digite o valor para sacar na conta de ",i[0])
+                    valor = float(input("\nValor: "))
+                    if valor > i[6]:
+                        print("\nValor indisponível.")
+                    else:
+                        i[6] -= valor
+                        print(f"Novo saldo: {i[6]}")
+    elif acao == 5:
+        print("Saindo...")
+        break
