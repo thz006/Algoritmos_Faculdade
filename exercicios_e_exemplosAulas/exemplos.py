42e7391c4ab827f4d74f055a605e6c8dc2aa475d
Arthur Santoro <arthursantorogomes92@gmail.com>
2025-05-13 19:11:45 -0400

    wwwwwwwww

--- exercicios_e_exemplosAulas/exemplos.py
+++ exercicios_e_exemplosAulas/exemplos.py
@@ -1,58 +1,43 @@
-listaClientes=[]
+cand1=[]
+cand2=[]
+cand3=[]
+eleitores=int(input("digite o numero de eleitores: "))
+for i in range(0,eleitores):
+    def votar():
+        print("candidato1=1\ncandidato2=2\ncandidato3=3")
+        voto=int(input("voto: "))
+        if voto == 1:
+            cand1.append(voto)
+        elif voto == 2:
+            cand2.append(voto)
+        elif voto == 3:
+            cand3.append(voto)
+        else:
+            print("numero inválido")
+            votar()
+    votar()
+if cand1 > cand2 and cand1 > cand3:
+    print("campeão: candidado 1")
+elif cand2 > cand1 and cand2 > cand3:
+    print("campeão: candidado 2")
+elif cand3 > cand1 and cand3 > cand2:
+    print("campeão: candidado 3")
+elif cand1 == cand2 and cand1 == cand3:
+    print("empate dos 3")
+# elif cand1 == cand2:    INCOMPLETO
+#     print("Segundo Turno entre candidato 2 e 3!")
+#     cand1t2=[]
+#     cand2t2=[]
+#     def votar():
+#         print("candidato1=1\ncandidato2=2")
+#         voto=int(input("voto: "))
+#         if voto == 1:
+#             cand1t2.append(voto)
+#         elif voto == 2:
+#             cand2t2.append(voto)
+#         else:
+#             print("numero inválido")
+#             votar()
+#     votar()
 
-print("\nBem Vindo ao Banco!\n")
-while True:
-    print("Ações disponíveis:\n \n 1- Cadastrar Cliente\n 2- Consultar saldo\n 3- Fazer depósito\n 4- Fazer saque\n 5- Sair.")
-    print("digite o número respectivo a ação desejada\n")
-    acao = int(input("Ação: "))
-    if acao == 1:
-        Cliente=[]
-        print("\nPara cadastrar o cliente, informe os seguintes dados: \n")
-        nome = input("Nome: ")
-        cpf = int(input("CPF: "))
-        rg = int(input("RG: "))
-        telefone = int(input("Telefone: "))
-        NumAgencia = int(input("Número da agência: "))
-        NumConta = int(input("Número conta: "))
-        saldo = float(input("Saldo Inicial: "))
-        Cliente.append(nome)
-        Cliente.append(cpf)
-        Cliente.append(rg)
-        Cliente.append(telefone)
-        Cliente.append(NumAgencia)
-        Cliente.append(NumConta)
-        Cliente.append(saldo)
-        listaClientes.append(Cliente)
-        print(f"Clientes cadastrados: \n{listaClientes}")
-        print(f"\n{nome}, seu cadastro foi realizado com sucesso!")
-    elif acao == 2:
-        print("Digite o CPF do cliente que você deseja consultar o saldo.\n")
-        buscaCPF = int(input("CPF: "))
-        for i in listaClientes:
-            for cpf in i:
-                if cpf == buscaCPF:
-                    print(f"Saldo de {i[0]}: {i[6]} reais")
-    elif acao == 3:
-        print("Digite o CPF da conta que você deseja efetuar o depósito.\n")
-        buscaCPF = int(input("CPF: "))
-        for i in listaClientes:
-            for cpf in i:
-                if cpf == buscaCPF:
-                    print(f"Digite o valor para depositar na conta de ",i[0])
-                    valor = float(input("\nValor: "))
-                    i[6] += valor
-                    print(f"Novo saldo: {i[6]}")
-    elif acao == 4:
-        for i in listaClientes:
-            for cpf in i:
-                if cpf == buscaCPF:
-                    print(f"Digite o valor para sacar na conta de ",i[0])
-                    valor = float(input("\nValor: "))
-                    if valor > i[6]:
-                        print("\nValor indisponível.")
-                    else:
-                        i[6] -= valor
-                        print(f"Novo saldo: {i[6]}")
-    elif acao == 5:
-        print("Saindo...")
-        break
+print(f"candidato 1: {len(cand1)} votos\ncandidato 2: {len(cand2)} votos\ncandidato 3: {len(cand3)} votos")
