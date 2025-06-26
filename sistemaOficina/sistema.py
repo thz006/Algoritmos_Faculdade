db02ef1fbe4d7b843c6545b73f3397a31928c400
Arthur Santoro <arthursantorogomes92@gmail.com>
2025-06-25 21:49:41 -0400

    atualiza

--- sistemaOficina/sistema.py
+++ sistemaOficina/sistema.py
@@ -1,4 +1,4 @@
-listaDeClientes=()
+listaDeClientes=[]
 def cadastrarCliente():
     while True:
         try:
@@ -19,12 +19,57 @@ def cadastrarCliente():
         except ValueError:
             print("Telefone inválido")
 
-    cliente={'Nome': nome, 'cpf': cpf, 'telefone': telefone}
-
+    cliente={'Nome': nome, 'cpf': cpf, 'telefone': telefone, 'veiculo': "", }
+    listaDeClientes.append(cliente)
     with open("sistemaOficina/clientes.txt", "a", encoding="utf-8") as arquivo:
-        arquivo.write(str(cliente))
+        arquivo.write(str(f"\n{cliente}"))
 
-    print("usuario cadastrado com sucesso.")
+    print("\nusuario cadastrado com sucesso.")
+
+    print(listaDeClientes)
+
+def cadastrarVeiculo():
+    while True:
+        try:
+            placa=input("digite a placa: ")
+            break
+        except:
+            print("placa inválida.")
+    while True:
+        try:
+            modelo=input("digite o modelo: ")
+            break
+        except:
+            print("modelo inválido")
+    while True:
+        try:
+            ano=int(input("digite o ano do veículo: "))
+            break
+        except ValueError:
+            print("Ano inválido")
+    veiculovar= (f"placa: {placa}, modelo: {modelo}, ano: {ano}")
+    veiculo={'placa': placa, 'modelo': modelo, 'ano': ano}
+
+    
+
+    while True:
+        try:
+            cpf=int(input("digite o CPF do proprietário: "))
+            for cliente in listaDeClientes:
+                if cliente['cpf'] == cpf:
+                    cliente['veiculo'] = veiculovar
+                    print(cliente)
+                    with open("sistemaOficina/veiculos.txt", "a", encoding="utf-8") as arquivo:
+                        arquivo.write(str(f"\n{veiculo}"))
+                        
+                    with open("sistemaOficina/cliente.txt", "w", encoding="utf-8") as arquivo:
+                        arquivo.write(str(f"\n{cliente}"))
+        except ValueError:
+            print("CPF inválido.")
+
+
+
+
 
 while True:
     def sistema():
@@ -32,5 +77,11 @@ while True:
         print("Escolha uma opção de Ação: \n1.Cadastro\n2.Buscar dados\n3.Editar\n4.Excluir\n5.Sair\n")
         acao = int(input("digite a ação desejada: "))
         if acao == 1:
-            cadastrarCliente()
+            print("Escolha oque você deseja cadastrar:\n1.Cadastrar cliente\n2.Cadastrar Veículo\n3.Cadastrar Ordem de Serviço\n4.Sair")
+            acao2=int(input("Escolha: "))
+            if acao2 == 1:
+                cadastrarCliente()
+            elif acao2 == 2:
+                cadastrarVeiculo()
+            
     sistema()
